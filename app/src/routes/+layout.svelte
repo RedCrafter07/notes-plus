bbdbbc88f265d6cf6ae054cb46742d47de7f32c3
RedCrafter07 <gh@r07.dev>
2025-07-16 21:47:17 +0200

    feat(ui): Add theming & catppuccin mocha as default theme

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
   import "../app.css";
+  import "../mocha.css";
 
   const { children } = $props();
 </script>
--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
   import { invoke } from "@tauri-apps/api/core";
-
-  async function greet(event: Event) {
-    event.preventDefault();
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    greetMsg = await invoke("greet", { name });
-  }
 </script>
 
-<h1 class="text-3xl">Welcome to RedNotes Plus!</h1>
-<h3 class="text-xl">What would you like to work on?</h3>
+<main class="w-full min-h-screen bg-base-1 text-content-1">
+  <div class="container mx-auto p-2">
+    <h1 class="text-3xl font-display">Welcome to RedNotes Plus!</h1>
+    <h3 class="text-xl">What would you like to work on?</h3>
+  </div>
+</main>
