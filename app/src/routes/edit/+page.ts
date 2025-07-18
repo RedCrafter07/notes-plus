086be20c627b13a7c5cef3f15a829f4c810297a9
RedCrafter07 <gh@r07.dev>
2025-07-18 16:06:48 +0200

    refactor(edit): Use a query parameter instead of a path in the url directly

---
+++ app/src/routes/edit/+page.ts
@@ -0,0 +1,20 @@
+import type { Note } from "$lib/types/bindings/Note";
+import { redirect } from "@sveltejs/kit";
+import type { PageLoad } from "./$types";
+import { invoke } from "@tauri-apps/api/core";
+
+export const load: PageLoad = async ({ url }) => {
+  const param = url.searchParams.get("path");
+
+  if (!param) {
+    return redirect(307, "/");
+  }
+
+  const pathString = decodeURIComponent(param);
+
+  const data = await invoke<Note>("load", { path: pathString });
+
+  console.log(data);
+
+  return data;
+};
