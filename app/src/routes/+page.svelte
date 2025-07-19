12bea37e2b353a932a9831b0dac71f3846e2d243
RedCrafter07 <gh@r07.dev>
2025-07-19 17:15:57 +0200

    feat(home): Rough layout draft

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
-  import { invoke } from "@tauri-apps/api/core";
+  import { IconSettings } from "@tabler/icons-svelte";
 </script>
 
-<main class="w-full">
+<main class="w-full flex-col gap-4">
+  <div class="bg-base-2 w-full py-2 px-4 items-center flex flex-row gap-2">
+    <div class="text-xl font-display font-black">RedNotes Plus</div>
+    <div class="ml-auto p-2 rounded-xl bg-base-3 aspect-square">
+      <IconSettings size={24} />
+    </div>
+  </div>
   <div class="container mx-auto p-2">
     <h1 class="text-3xl font-display">Welcome to RedNotes Plus!</h1>
     <h3 class="text-xl">What would you like to work on?</h3>
