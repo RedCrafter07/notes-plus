a9e4b4b108da96131649cea0923bc2eefa35f649
RedCrafter07 <gh@r07.dev>
2025-07-22 16:47:08 +0200

    chore(design): Add some small animations to the settings icon

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -2,11 +2,16 @@
   import { IconSettings } from "@tabler/icons-svelte";
 </script>
 
-<main class="w-full flex-col gap-4">
+<main class="w-full flex-col gap-4 select-none">
   <div class="bg-base-2 w-full py-2 px-4 items-center flex flex-row gap-2">
     <div class="text-xl font-display font-black">RedNotes Plus</div>
-    <div class="ml-auto p-2 rounded-xl bg-base-3 aspect-square">
-      <IconSettings size={24} />
+    <div
+      class="ml-auto p-2 rounded-xl bg-base-3 aspect-square active:scale-90 active:opacity-90 transition-all group"
+    >
+      <IconSettings
+        size={24}
+        class="group-hover:opacity-100 opacity-90 transition-opacity"
+      />
     </div>
   </div>
   <div class="container mx-auto p-2">
