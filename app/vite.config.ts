39f16bd77312acf1aef42b251f9185e3680f709c
RedCrafter07 <gh@r07.dev>
2025-07-22 16:47:08 +0200

    refactor: Make vite config a TypeScript file

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -5,7 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
-// https://vitejs.dev/config/
 export default defineConfig(async () => ({
   plugins: [tailwindcss(), sveltekit()],
 
