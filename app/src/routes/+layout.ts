48e4bdd073de0610423f3cc869d28f096ab4f291
RedCrafter07 <gh@r07.dev>
2025-08-30 14:08:26 +0200

    fix(layout): Remove prefs loader from +layout.ts
    -> This doesn't work

--- app/src/routes/+layout.ts
+++ app/src/routes/+layout.ts
@@ -1,19 +1,5 @@
-
-import type { UserPrefs } from "$lib/types/bindings/UserPrefs";
-import { invoke } from "@tauri-apps/api/core";
-
 // Tauri doesn't have a Node.js server to do proper SSR
 // so we will use adapter-static to prerender the app (SSG)
 // See: https://v2.tauri.app/start/frontend/sveltekit/ for more info
 export const prerender = true;
 export const ssr = false;
-
-export const load = async () => {
-  const prefs = await getPrefs();
-
-  return { prefs };
-}
-
-async function getPrefs() {
-  return await invoke<UserPrefs>("get_prefs");
-}
