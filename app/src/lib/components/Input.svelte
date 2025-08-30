98fbe9c4dcfcdc7a5ad72a05e8de44ee396d0f22
RedCrafter07 <gh@r07.dev>
2025-08-30 14:41:44 +0200

    refactor(ui): Add input component

---
+++ app/src/lib/components/Input.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+  import type { FocusEventHandler } from "svelte/elements";
+
+  const {
+    label,
+    description,
+    value,
+    id,
+    placeholder,
+    onBlur,
+    children,
+  }: {
+    value?: string;
+    id: string;
+    label: string;
+    description?: string;
+    placeholder?: string;
+    onBlur?: FocusEventHandler<HTMLInputElement>;
+    children?: Snippet<[]>;
+  } = $props();
+</script>
+
+<label for={`#${id}`} class="text-xl">{label}</label>
+
+{#if description}
+  <p class="text-content-1/75">
+    {description}
+  </p>
+{/if}
+<div class="flex flex-row gap-2 w-full py-2 px-3 rounded-xl bg-base-3">
+  <input
+    type="text"
+    class="w-full flex-1 focus:outline-none"
+    {value}
+    onblur={onBlur}
+    {id}
+    {placeholder}
+  />
+  {@render children?.()}
+</div>
--- app/src/lib/components/Prefs.svelte
+++ app/src/lib/components/Prefs.svelte
@@ -3,9 +3,8 @@
   import { IconDeviceFloppy, IconFolder, IconX } from "@tabler/icons-svelte";
   import { invoke } from "@tauri-apps/api/core";
   import { open } from "@tauri-apps/plugin-dialog";
-  import { linear } from "svelte/easing";
-  import { fade } from "svelte/transition";
   import Overlay from "./Overlay.svelte";
+  import Input from "./Input.svelte";
 
   const { closeCb, visible }: { closeCb: () => void; visible: boolean } =
     $props();
@@ -32,22 +31,16 @@
     <div class="flex flex-col gap-2">
       <h3 class="text-2xl">Storage Settings</h3>
       <div>
-        <label for="#document-dir" class="text-xl">Document Directory</label>
-        <p class="text-content-1/75">
-          This is the main place your notes get stored. Imagine it like a folder
-          containing your notebook.
-        </p>
-        <div class="flex flex-row gap-2 w-full py-2 px-3 rounded-xl bg-base-3">
-          <input
-            type="text"
-            id="document-dir"
-            class="w-full flex-1 focus:outline-none"
-            value={prefs?.default_document_dir}
-            onblur={(e) => {
-              if (!prefs) return;
-              prefs.default_document_dir = e.currentTarget.value;
-            }}
-          />
+        <Input
+          id="document-dir"
+          label="Document Directory"
+          description="This is the main place your notes get stored. Imagine it like a folder containing your notebook."
+          value={prefs?.default_document_dir}
+          onBlur={(e) => {
+            if (!prefs) return;
+            prefs.default_document_dir = e.currentTarget.value;
+          }}
+        >
           <button
             class="aspect-square h-full rounded-xl text-content-1/50 hover:text-content-1 active:scale-95 transition-all"
             type="button"
@@ -69,24 +62,19 @@
           >
             <IconFolder />
           </button>
-        </div>
+        </Input>
       </div>
       <div>
-        <label for="#workdir" class="text-xl">Working Directory</label>
-        <p class="text-content-1/75">
-          This is simply for temporary files; they will get deleted afterwards.
-        </p>
-        <div class="flex flex-row gap-2 w-full py-2 px-3 rounded-xl bg-base-3">
-          <input
-            type="text"
-            id="workdir"
-            class="w-full flex-1 focus:outline-none"
-            value={prefs?.workdir}
-            onblur={(e) => {
-              if (!prefs) return;
-              prefs.workdir = e.currentTarget.value;
-            }}
-          />
+        <Input
+          id="workdir"
+          label="Working Directory"
+          description="This is simply for temporary files; they will get deleted afterwards."
+          value={prefs?.workdir}
+          onBlur={(e) => {
+            if (!prefs) return;
+            prefs.workdir = e.currentTarget.value;
+          }}
+        >
           <button
             class="aspect-square h-full rounded-xl text-content-1/50 hover:text-content-1 active:scale-95 transition-all"
             type="button"
@@ -97,7 +85,7 @@
                 directory: true,
                 defaultPath: prefs.workdir,
                 canCreateDirectories: true,
-                title: "Select the primary document directory.",
+                title: "Select the working directory.",
                 multiple: false,
               });
 
@@ -108,7 +96,7 @@
           >
             <IconFolder />
           </button>
-        </div>
+        </Input>
       </div>
       <button
         class="flex flex-row gap-4 justify-center w-max border-success text-success border hover:text-success-content hover:bg-success transition-all py-2 px-3 rounded-xl cursor-pointer"
