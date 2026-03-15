5f91e84cd8cdc5f61f0513ec9212bf182ee0e0a3
RedCrafter07 <gh@r07.dev>
2026-03-15 18:58:12 +0100

    feat(overlay): Reduce blur effect

--- app/src/lib/components/Overlay.svelte
+++ app/src/lib/components/Overlay.svelte
@@ -28,7 +28,7 @@
       <!-- svelte-ignore a11y_click_events_have_key_events -->
       <!-- svelte-ignore a11y_no_static_element_interactions -->
       <div
-        class="absolute top-0 pointer-events-auto w-full h-full backdrop-blur-3xl bg-base-1/50"
+        class="absolute top-0 pointer-events-auto w-full h-full backdrop-blur-sm bg-base-1/50"
         onclick={closeCb}
       ></div>
 
