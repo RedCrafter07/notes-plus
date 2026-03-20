6ac665fa723e9efb0dd95f7d11d7cecc911a5b21
RedCrafter07 <gh@r07.dev>
2026-03-20 18:02:54 +0100

    feat(tabs): enhance tab management with scroll functionality and unsaved state handling

--- app/src/lib/components/Tabs.svelte
+++ app/src/lib/components/Tabs.svelte
@@ -2,7 +2,19 @@
   import { goto } from "$app/navigation";
   import { resolve } from "$app/paths";
   import { tabManager } from "$lib/state/tabManager.svelte";
-  import { IconHome } from "@tabler/icons-svelte";
+  import { IconHome, IconX } from "@tabler/icons-svelte";
+
+  let tabs = $state<Record<string, HTMLDivElement>>({});
+
+  function assignTab(element: HTMLDivElement, id: string) {
+    tabs[id] = element;
+  }
+
+  $effect(() => {
+    if (!tabManager.activeNote) return;
+    const t = tabs[tabManager.activeNote.id];
+    if (t) t.scrollIntoView({ behavior: "instant" });
+  });
 
   function updateTab(index: number) {
     tabManager.activeTab = index;
@@ -11,7 +23,7 @@
 
 <div
   class={[
-    "flex gap-2 flex-row  w-full bg-base-2 overflow-y-hidden px-2 transition-all",
+    "flex gap-2 flex-row w-full bg-base-2 overflow-hidden px-2 transition-all",
     {
       "h-14 py-2": tabManager.tabs.length > 0,
       "h-0 py-0": tabManager.tabs.length === 0,
@@ -19,8 +31,9 @@
   ]}
 >
   <button
+    id="tab-bar"
     class={[
-      "aspect-square h-full transition-all cursor-pointer flex items-center justify-center text-content-2 hover:text-content-1 rounded-xl",
+      "aspect-square h-full transition-all cursor-pointer flex items-center justify-center text-content-2 hover:text-content-1 rounded-xl sticky left-0",
       {
         "bg-overlay/5 hover:bg-overlay/20": tabManager.activeTab !== -1,
         "bg-overlay/10 hover:bg-overlay/20": tabManager.activeTab === -1,
@@ -33,24 +46,57 @@
   >
     <IconHome />
   </button>
-  {#each tabManager.tabs as tab, i (`tabs-${tab.id}`)}
-    {@const active = tabManager.activeTab === i}
-    <button
-      class={[
-        "h-full flex-1 transition-all rounded-xl",
-        {
-          "bg-overlay/5 hover:bg-overlay/20 text-content-2 hover:text-content-1":
-            !active,
-          "bg-overlay/10 hover:bg-overlay/20 text-content-2 hover:text-content-1":
-            active,
-        },
-      ]}
-      onclick={() => {
-        updateTab(i);
-        goto(resolve("/edit/[id]", { id: tabManager.tabs[i].id }));
-      }}
-    >
-      {tab.content.title}
-    </button>
-  {/each}
+  <div
+    id="tab-bar"
+    class="flex gap-2 flex-row w-full h-full bg-base-2 overflow-y-hidden transition-all overflow-x-auto relative"
+    onwheel={(e) => {
+      const tabBarElement = e.currentTarget;
+      if (!tabBarElement) return;
+      e.preventDefault();
+
+      tabBarElement.scrollLeft += e.deltaY + e.deltaX;
+    }}
+  >
+    {#each tabManager.tabs as tab, i (`tabs-${tab.id}`)}
+      {@const active = tabManager.activeTab === i}
+      <div
+        class={[
+          "h-full flex-1 transition-all rounded-xl p-2 flex flex-row gap-2 min-w-max z-0",
+          {
+            "bg-overlay/5 hover:bg-overlay/20 text-content-2 hover:text-content-1":
+              !active,
+            "bg-overlay/10 hover:bg-overlay/20 text-content-2 hover:text-content-1":
+              active,
+          },
+        ]}
+        role="tab"
+        use:assignTab={tab.id}
+      >
+        <button
+          class="flex-1 flex items-center w-full h-full min-w-max"
+          onpointerdown={(e) => {
+            if (e.button !== 1) return;
+            e.preventDefault();
+            tabManager.remove(i);
+          }}
+          onclick={() => {
+            updateTab(i);
+            goto(resolve("/edit/[id]", { id: tabManager.tabs[i].id }));
+          }}
+        >
+          <p class="min-w-max">
+            {`${tab.content.title}${tab.unsaved ? " *" : ""}`}
+          </p>
+        </button>
+        <button
+          class="opacity-50 hover:opacity-100 hover:text-destructive transition-all"
+          onclick={() => {
+            tabManager.remove(i);
+          }}
+        >
+          <IconX />
+        </button>
+      </div>
+    {/each}
+  </div>
 </div>
--- app/src/lib/state/tabManager.svelte.ts
+++ app/src/lib/state/tabManager.svelte.ts
@@ -1,19 +1,30 @@
+import { goto } from "$app/navigation";
+import { resolve } from "$app/paths";
 import type { NoteData } from "$lib/tauri/bindings";
 import { contentManager } from "./contentManager.svelte";
 
 class TabManager {
-  #tabs = $state<NoteData[]>([]);
+  #tabs = $state<(NoteData & { unsaved: boolean })[]>([]);
   #activeTab = $state(-1);
 
   add(noteData: NoteData, setActive: boolean = false) {
-    const l = this.#tabs.push(noteData);
+    const l = this.#tabs.push({
+      ...noteData,
+      unsaved: false,
+    });
 
     if (setActive) {
       this.activeTab = l - 1;
+      goto(resolve("/edit/[id]", { id: noteData.id }));
     }
   }
 
-  async remove(index: number) {
+  remove(index: number, force = false) {
+    if (this.activeNote?.unsaved && !force) {
+      return;
+    }
+    this.#activeTab--;
+    if (this.#activeTab === -1) goto(resolve("/"));
     this.#tabs.splice(index, 1);
   }
 
@@ -21,14 +32,26 @@ class TabManager {
     return this.#activeTab;
   }
 
+  get activeNote() {
+    if (this.#activeTab === -1) return null;
+    return this.#tabs[this.#activeTab];
+  }
+
   set activeTab(tabIndex: number) {
     if (tabIndex < this.#tabs.length && tabIndex >= 0) {
-      this.#tabs[this.#activeTab] = contentManager.export();
+      this.#tabs[this.#activeTab] = {
+        ...contentManager.export(),
+        unsaved: this.activeNote?.unsaved ?? false,
+      };
 
       this.#activeTab = tabIndex;
 
-      contentManager.import(tabManager.tabs[tabManager.activeTab]);
+      contentManager.import(this.activeNote!);
     }
+  }
+
+  setEdited() {
+    this.#tabs[this.#activeTab].unsaved = true;
   }
 
   get tabs() {
