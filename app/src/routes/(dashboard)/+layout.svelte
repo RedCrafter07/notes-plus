efb577d1047ffb72500cc2fe26b2bcfa4990f514
RedCrafter07 <gh@r07.dev>
2026-03-22 21:41:58 +0100

    refactor(layout): Improve button structure and navigation handling

--- app/src/routes/(dashboard)/+layout.svelte
+++ app/src/routes/(dashboard)/+layout.svelte
@@ -1,17 +1,24 @@
 <script lang="ts">
+  import { goto } from "$app/navigation";
+  import { resolve } from "$app/paths";
   import { newNote } from "$lib/util/notes";
   import { IconHome, IconPlus, IconSettings } from "@tabler/icons-svelte";
 
   let { children } = $props();
 </script>
 
-<div class="flex flex-row gap-2 w-full min-h-screen bg-base-1">
+<div class="flex flex-row w-full min-h-screen bg-base-1">
   <div class="bg-base-2 flex flex-col gap-8 p-4">
     <div>
       <h3 class="font-display text-2xl font-black">RedNotes Plus</h3>
     </div>
     <div class="flex flex-col gap-4">
-      <button class="flex flex-row gap-2 items-center">
+      <button
+        class="flex flex-row gap-2 items-center"
+        onclick={() => {
+          goto(resolve("/"));
+        }}
+      >
         <IconHome />
         <p>Home</p>
       </button>
@@ -24,13 +31,18 @@
         <IconPlus />
         <p>New Note</p>
       </button>
-      <button class="flex flex-row gap-2 items-center" onclick={async () => {}}>
+      <button
+        class="flex flex-row gap-2 items-center"
+        onclick={() => {
+          goto(resolve("/settings"));
+        }}
+      >
         <IconSettings />
         <p>Settings</p>
       </button>
     </div>
   </div>
-  <div class="flex-1">
+  <div class="flex-1 p-2">
     {@render children()}
   </div>
 </div>
