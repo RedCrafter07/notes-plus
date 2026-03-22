64c8ae33d147b03f971dd3680ed64a650a31d198
RedCrafter07 <gh@r07.dev>
2026-03-22 15:23:56 +0100

    feat(layout): Add basic sidebar

---
+++ app/src/routes/(dashboard)/+layout.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import { newNote } from "$lib/util/notes";
+  import { IconHome, IconPlus, IconSettings } from "@tabler/icons-svelte";
+
+  let { children } = $props();
+</script>
+
+<div class="flex flex-row gap-2 w-full min-h-screen bg-base-1">
+  <div class="bg-base-2 flex flex-col gap-8 p-4">
+    <div>
+      <h3 class="font-display text-2xl font-black">RedNotes Plus</h3>
+    </div>
+    <div class="flex flex-col gap-4">
+      <button class="flex flex-row gap-2 items-center">
+        <IconHome />
+        <p>Home</p>
+      </button>
+      <button
+        class="flex flex-row gap-2 items-center"
+        onclick={async () => {
+          await newNote();
+        }}
+      >
+        <IconPlus />
+        <p>New Note</p>
+      </button>
+      <button class="flex flex-row gap-2 items-center" onclick={async () => {}}>
+        <IconSettings />
+        <p>Settings</p>
+      </button>
+    </div>
+  </div>
+  <div class="flex-1">
+    {@render children()}
+  </div>
+</div>
