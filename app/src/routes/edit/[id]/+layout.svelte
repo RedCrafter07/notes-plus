8e052c51d8ec7b4bd86820f530b61da9a033dd9a
RedCrafter07 <gh@r07.dev>
2026-03-23 17:14:43 +0100

    feat(editor): Add error boundry to throw user back to home screen

---
+++ app/src/routes/edit/[id]/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { goto } from "$app/navigation";
+  import { resolve } from "$app/paths";
+
+  const { children } = $props();
+</script>
+
+<svelte:boundary
+  onerror={() => {
+    goto(resolve("/"));
+  }}
+>
+  {@render children()}
+</svelte:boundary>
