b2b3dc9cc87e8fa5b65fdfb499c90d3acb06e30d
RedCrafter07 <gh@r07.dev>
2026-03-24 19:32:05 +0100

    feat(editor): Enhance save functionality for notes with improved handling of unsaved changes

--- app/src/routes/edit/[id]/+page.svelte
+++ app/src/routes/edit/[id]/+page.svelte
@@ -87,8 +87,32 @@
     const event = async (e: KeyboardEvent) => {
       if (!e.ctrlKey) return;
 
-      if (e.key === "S" || (e.key === "s" && e.shiftKey)) {
-        await commands.saveNoteDialog(contentManager.export());
+      if (e.key === "s" && !e.shiftKey && contentManager.path !== null) {
+        const saveSuccess = await commands.saveNote(contentManager.export());
+        if (saveSuccess) {
+          if (tabManager.activeNote) tabManager.activeNote.unsaved = false;
+        } else {
+          // TODO: Display error
+        }
+      }
+      if (
+        e.key === "S" ||
+        (e.key === "s" && e.shiftKey) ||
+        (e.key === "s" && !e.shiftKey && contentManager.path === null)
+      ) {
+        if (!tabManager.activeNote) return;
+        const id = contentManager.path
+          ? crypto.randomUUID()
+          : contentManager.id;
+
+        contentManager.id = id;
+
+        const path = await commands.saveNoteDialog(contentManager.export());
+
+        if (path) {
+          contentManager.path = path;
+          if (tabManager.activeNote) tabManager.activeNote.unsaved = false;
+        }
       }
     };
     document.addEventListener("keydown", event);
