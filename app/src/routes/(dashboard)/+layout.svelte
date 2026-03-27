b52134162af9fc39173b786fa257771e1c442ac1
RedCrafter07 <gh@r07.dev>
2026-03-27 22:11:33 +0100

    feat(layout): Add button to open files

--- app/src/routes/(dashboard)/+layout.svelte
+++ app/src/routes/(dashboard)/+layout.svelte
@@ -1,8 +1,15 @@
 <script lang="ts">
   import { goto } from "$app/navigation";
   import { resolve } from "$app/paths";
+  import { tabManager } from "$lib/state/tabManager.svelte";
+  import { commands } from "$lib/tauri/bindings";
   import { newNote } from "$lib/util/notes";
-  import { IconHome, IconPlus, IconSettings } from "@tabler/icons-svelte";
+  import {
+    IconFolder,
+    IconHome,
+    IconPlus,
+    IconSettings,
+  } from "@tabler/icons-svelte";
 
   let { children } = $props();
 </script>
@@ -30,6 +37,20 @@
       >
         <IconPlus />
         <p>New Note</p>
+      </button>
+      <button
+        class="flex flex-row gap-2 items-center"
+        onclick={async () => {
+          const data = await commands.openNotesDialog();
+
+          data.forEach((d, i, a) => {
+            tabManager.add(d, i === a.length - 1);
+            if (i === a.length - 1) goto(resolve("/edit/[id]", { id: d.id }));
+          });
+        }}
+      >
+        <IconFolder />
+        <p>Open files</p>
       </button>
       <button
         class="flex flex-row gap-2 items-center"
