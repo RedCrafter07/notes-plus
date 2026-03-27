09a314910c5cbd4bc35d41297726f473ad9d7aa0
RedCrafter07 <gh@r07.dev>
2026-03-27 23:01:00 +0100

    feat(components): Add ColorPicker component for color selection

---
+++ app/src/lib/components/ColorPicker.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  let {
+    value = $bindable(),
+  }: {
+    value: string;
+  } = $props();
+</script>
+
+<div class="relative w-6 h-6">
+  <div
+    class="absolute inset-0 rounded-full border-content-3 border-2 pointer-events-none"
+    style="background-color: {value};"
+  ></div>
+
+  <input
+    type="color"
+    class="absolute inset-0 opacity-0 w-full h-full cursor-pointer"
+    bind:value
+    aria-label="Choose color"
+  />
+</div>
