043fa91d85e2c93b08b099fbf7985c78bd3db5e8
RedCrafter07 <gh@r07.dev>
2026-05-22 23:53:28 +0200

    feat(editor): Add editor sidebar

---
+++ app/src/lib/components/EditorBar.svelte
@@ -0,0 +1,90 @@
+<script lang="ts">
+  import { contentManager } from "$lib/state/contentManager.svelte";
+  import { editorComponents } from "$lib/state/editorComponents.svelte";
+  import { overlayManager } from "$lib/state/overlayManager.svelte";
+  import { IconSettings } from "@tabler/icons-svelte";
+  import { fade } from "svelte/transition";
+</script>
+
+<div
+  class={[
+    "overflow-x-hidden transition-all bg-base-1 py-2 h-full shrink-0 flex flex-col gap-4",
+    {
+      "w-1/4 px-2": editorComponents.rightBarOpen,
+      "w-0": !editorComponents.rightBarOpen,
+    },
+  ]}
+>
+  <div class="w-full p-2 rounded-xl bg-base-2 text-center">
+    <div class="w-full h-max relative flex flex-row tracking-wide">
+      <div
+        class={[
+          "rounded-xl bg-overlay/10 absolute h-full w-1/2 transition-all duration-100",
+          {
+            "left-0": editorComponents.rightBarOption === "pages",
+            "left-1/2": editorComponents.rightBarOption === "layers",
+          },
+        ]}
+      ></div>
+      <button
+        class="rounded-xl p-1 flex-1"
+        onclick={() => {
+          editorComponents.rightBarOption = "pages";
+        }}>Pages</button
+      >
+      <button
+        class="rounded-xl p-1 flex-1"
+        onclick={() => {
+          editorComponents.rightBarOption = "layers";
+        }}>Layers</button
+      >
+    </div>
+  </div>
+
+  <div class="relative w-full h-full">
+    {#key editorComponents.rightBarOption}
+      <div
+        transition:fade={{ duration: 100 }}
+        class="absolute top-0 h-full w-full text-center"
+      >
+        {#if editorComponents.rightBarOption === "pages"}
+          <div class="grid grid-cols-2 w-full">
+            {#each contentManager.pages as page, i (`page-${i}`)}
+              <!-- svelte-ignore a11y_click_events_have_key_events -->
+              <div
+                onclick={() => {
+                  contentManager.activePage = i;
+                }}
+                class={[
+                  "p-2 rounded-xl border-2 bg-base-2 w-full cursor-pointer",
+                  contentManager.activePage === i
+                    ? "border-accent"
+                    : "border-transparent",
+                ]}
+                role="button"
+                tabindex="0"
+              >
+                <div></div>
+                <div class="flex flex-row items-center justify-between">
+                  <p>
+                    {page.name}
+                  </p>
+                  <button
+                    onclick={() => {
+                      overlayManager.setOpen(`pagesettings-${i}`);
+                    }}
+                    class="text-content-3 hover:text-content-1 active:scale-90 transition-all"
+                  >
+                    <IconSettings />
+                  </button>
+                </div>
+              </div>
+            {/each}
+          </div>
+        {:else}
+          <h3 class="text-xl">Todo</h3>
+        {/if}
+      </div>
+    {/key}
+  </div>
+</div>
--- app/src/routes/edit/[id]/+layout.svelte
+++ app/src/routes/edit/[id]/+layout.svelte
@@ -1,15 +1,21 @@
 <script>
   import { goto } from "$app/navigation";
   import { resolve } from "$app/paths";
+  import EditorBar from "$lib/components/EditorBar.svelte";
 
   const { children } = $props();
 </script>
 
-<svelte:boundary
-  onerror={(e) => {
-    console.error(e);
-    goto(resolve("/"));
-  }}
->
-  {@render children()}
-</svelte:boundary>
+<div class="flex flex-row h-full relative">
+  <div class="flex-1 h-full relative">
+    <svelte:boundary
+      onerror={(e) => {
+        console.error(e);
+        goto(resolve("/"));
+      }}
+    >
+      {@render children()}
+    </svelte:boundary>
+  </div>
+  <EditorBar />
+</div>
