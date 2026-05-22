2b52badf135516529b7292bdbdeaf0eb423b0317
RedCrafter07 <gh@r07.dev>
2026-05-22 23:53:28 +0200

    feat(components): Add color input A color picker that displays both the color as text and has the capability to open the system color picker for convenience.

---
+++ app/src/lib/components/ColorInput.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import type { FocusEventHandler } from "svelte/elements";
+  import ColorPicker from "./ColorPicker.svelte";
+  import Input from "./Input.svelte";
+
+  let {
+    id,
+    value = $bindable(),
+    ...rest
+  }: {
+    id: string;
+    value: string;
+    component?: HTMLInputElement;
+    label?: string;
+    description?: string;
+    placeholder?: string;
+    onBlur?: FocusEventHandler<HTMLInputElement>;
+    tabIndex?: number;
+  } = $props();
+</script>
+
+<Input {id} bind:value {...rest}>
+  <ColorPicker bind:value />
+</Input>
