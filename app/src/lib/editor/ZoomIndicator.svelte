387c46758bcce7dd5519ca1a9a10c24d09dee97f
RedCrafter07 <gh@r07.dev>
2026-05-24 01:08:27 +0200

    feat(editor): Make zoom indicator more useful

--- app/src/lib/editor/ZoomIndicator.svelte
+++ app/src/lib/editor/ZoomIndicator.svelte
@@ -2,6 +2,17 @@
   import { contentManager } from "$lib/state/contentManager.svelte";
 </script>
 
-<div class="bottom-4 left-4 absolute rounded-xl p-2 z-10 text-black h-max">
+<button
+  class={[
+    "bottom-4 left-4 absolute rounded-xl p-2 z-10 bg-primary text-base-1 h-max active:scale-90 transition-all",
+    {
+      "opacity-0 pointer-events-none": contentManager.zoom === 1,
+      "opacity-100 pointer-events-auto": contentManager.zoom !== 1,
+    },
+  ]}
+  onclick={() => {
+    contentManager.zoom = 1;
+  }}
+>
   {Math.round(contentManager.zoom * 10000) / 100}%
-</div>
+</button>
