3e89f1f65dc244deccb225bf5ee549e96ec9f365
RedCrafter07 <gh@r07.dev>
2026-07-03 18:20:31 +0200

    feat(archive): Make saves atomic
    The original file now doesn't get corrupted when the save errors or gets
    interrupted otherwise

--- common/src/io/archive.rs
+++ common/src/io/archive.rs
@@ -1,14 +1,20 @@
 use sevenz_rust2::{ArchiveEntry, ArchiveReader, ArchiveWriter, Password};
-use std::path::Path;
+use std::{ffi::OsString, fs::rename, path::Path};
 
 // TODO: Add functionality and attachment support
 // pub fn add_attachment() {}
-pub fn create_with_data(note_data: &[u8], path: &Path) -> Result<(), sevenz_rust2::Error> {
-    let mut archive_writer = ArchiveWriter::create(path)?;
+pub fn create_with_data(note_data: &[u8], path: &Path) -> anyhow::Result<()> {
+    let tmp_path = tmp_path_for(path);
 
+    let mut archive_writer = ArchiveWriter::create(&tmp_path)?;
     archive_writer.push_archive_entry(ArchiveEntry::new_file("data"), Some(note_data))?;
-
     archive_writer.finish()?;
+
+    if let Err(e) = rename(&tmp_path, path) {
+        std::fs::remove_file(&tmp_path)?;
+
+        return Err(e.into());
+    };
 
     Ok(())
 }
@@ -19,4 +25,15 @@ pub fn open_data(path: &Path) -> Result<Vec<u8>, sevenz_rust2::Error> {
     let file_content = archive_reader.read_file("data")?;
 
     Ok(file_content)
+}
+
+fn tmp_path_for(path: &Path) -> std::path::PathBuf {
+    let mut file_name = path
+        .file_name()
+        .map(|n| n.to_os_string())
+        .unwrap_or_else(|| OsString::from("note"));
+
+    file_name.push(format!(".tmp-{}", std::process::id()));
+
+    path.with_file_name(file_name)
 }
