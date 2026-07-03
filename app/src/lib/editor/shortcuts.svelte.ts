40fcef5d6785d6fa78bf6785075da105c75325cd
RedCrafter07 <gh@r07.dev>
2026-07-03 17:13:26 +0200

    fix(shortcuts): Fix bug where the file gets written twice

--- app/src/lib/editor/shortcuts.svelte.ts
+++ app/src/lib/editor/shortcuts.svelte.ts
@@ -8,52 +8,7 @@ export function useShortcuts() {
     const event = async (e: KeyboardEvent) => {
       if (!e.ctrlKey) return;
 
-      if (e.key === "s" && !e.shiftKey && tabManager.tab.path === undefined) {
-        const path = await commands.saveNoteToStorage(contentManager.export());
-
-        if (path) {
-          popupManager.add({
-            message: "Note saved successfully!",
-            type: "success",
-          });
-          if (tabManager.activeNote) {
-            tabManager.activeNote.unsaved = false;
-            tabManager.currentPath = path;
-          }
-        } else {
-          popupManager.add({
-            message: "An error occurred while saving the note.",
-            type: "destructive",
-          });
-        }
-      }
-      if (e.key === "s" && !e.shiftKey && tabManager.tab.path !== undefined) {
-        const saveSuccess = await commands.saveNote(
-          contentManager.export(),
-          tabManager.tab.path,
-        );
-        if (saveSuccess) {
-          if (tabManager.activeNote) tabManager.activeNote.unsaved = false;
-        } else {
-          // TODO: Display error
-        }
-      }
-      if (e.key === "S" || (e.key === "s" && e.shiftKey)) {
-        if (!tabManager.activeNote) return;
-        const id = tabManager.tab.path
-          ? crypto.randomUUID()
-          : contentManager.id;
-
-        contentManager.id = id;
-
-        const path = await commands.saveNoteDialog(contentManager.export());
-
-        if (path) {
-          tabManager.currentPath = path;
-          console.log(tabManager.tab.path);
-          if (tabManager.activeNote) tabManager.activeNote.unsaved = false;
-        }
-      }
+      if (e.key.toLowerCase() === "s") await handleSave(e);
     };
     document.addEventListener("keydown", event);
 
@@ -61,4 +16,49 @@ export function useShortcuts() {
       document.removeEventListener("keydown", event);
     };
   });
+
+  async function handleSave(e: KeyboardEvent) {
+    e.preventDefault();
+    if (!e.shiftKey) {
+      const path = await commands.saveNoteToStorage(contentManager.export());
+
+      if (path) {
+        popupManager.add({
+          message: "Note saved successfully!",
+          type: "success",
+        });
+        if (tabManager.activeNote) {
+          tabManager.activeNote.unsaved = false;
+          tabManager.currentPath = path;
+        }
+      } else {
+        popupManager.add({
+          message: "An error occurred while saving the note.",
+          type: "destructive",
+        });
+      }
+    } else {
+      if (!tabManager.activeNote) return;
+      const lastId = contentManager.id;
+
+      // Save As: New file should get new ID.
+      const id = tabManager.tab.path ? crypto.randomUUID() : contentManager.id;
+
+      contentManager.id = id;
+
+      const path = await commands.saveNoteDialog(contentManager.export());
+
+      if (path) {
+        tabManager.currentPath = path;
+        if (tabManager.activeNote) tabManager.activeNote.unsaved = false;
+      } else {
+        // Revert ID, no saving since error occurred
+        contentManager.id = lastId;
+        popupManager.add({
+          message: "An error occurred while saving the file. Please try again",
+          type: "destructive",
+        });
+      }
+    }
+  }
 }
